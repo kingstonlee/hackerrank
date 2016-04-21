acb689c5a530b76a06191c123dec249417e6be91
kingstonlee <kingstonlee@gmail.com>
2016-04-21 15:00:03 -0700

    Update botSavesPrincess.py

--- botSavesPrincess.py
+++ botSavesPrincess.py
@@ -26,15 +26,19 @@ def displayPathtoPrincess(n,grid):
     x = current_position[0] - princess_position[0]
     y = current_position[1] - princess_position[1]
     
-    #TODO: Not the correct way of adding multiples to a list
-    if x < 0:
-        moves.append('RIGHT' * abs(x))
-    elif x > 0:
-        moves.append('LEFT' * abs(x))
-    if y < 0:
-        moves.append('DOWN' * abs(y))
-    elif y > 0:
-        moves.append('UP' * abs(y))
+    while x != 0 and y != 0:
+        if x < 0:
+            moves.append('RIGHT')
+            x += 1
+        elif x > 0:
+            moves.append('LEFT')
+            x -= 1
+        if y < 0:
+            moves.append('DOWN')
+            y += 1
+        elif y > 0:
+            moves.append('UP')
+            y -= 1
     
     for move in moves:
         print(move)
